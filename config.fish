7fb2761335a3fd5c945517f4d1e7fec4d7ecdd9c
Ali Aminfar <ali.aminfar@gmail.com>
2019-07-05 15:55:13 +0000

    Adding fzf installer / uninstaller

--- config.fish
+++ config.fish
@@ -1,4 +1,10 @@
+set -gx fzf_dir $HOME/.fzf
+
 set -gx PATH /usr/local/bin /usr/local/sbin /usr/bin /usr/sbin /bin /sbin
+
+if test -d $fzf_dir/bin
+    set -gx PATH $HOME/bin $fzf_dir/bin $PATH
+end
 
 source $__fish_config_dir/local_config.fish
 
